37fcd633a7e11028e41571f1ebd566172917f40f
Clayton <cmpetersen@hotmail.co.za>
2019-09-25 10:28:21 +0200

    Adding the driver class Project still needs to be completed

---
+++ src/EmployeeDriver.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class EmployeeDriver {
+
+    private static Scanner userInput = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+
+}
